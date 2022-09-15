a96a182443c76072edbe0328838f76e6702f9e1a
Thulane Mfeketho <mfekethothulane@gmail.comm>
2022-09-15 09:39:57 +0200

    update

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ http.createServer(function(request, response){
     var pathname = url.parse(request.url).pathname;
 
     // PRINT THE NAME OF THE FILE FOR WHICH REQUEST IS MADE
-    console.log("Request for"+ pathname+"recived");
+    console.log("Request for "+ pathname+" recived");
 
     // READ THE REQUEST FILE  CONTENT FROM FILE SYSTEM
-    fs.readFile(pathname.substr(1), function (err, data) {
+    fs.readFile(pathname.substring(1), function (err, data) {
       if (err) {
         console.log(err);
         
